7ff3b5358b707e9db8027d5a2ab0500903a5c098
Johananjaratiana <johanandrianaivosoa@gmail.com>
2023-03-17 14:36:35 +0300

    Sprint 31 finis
    Sprint 31 finis

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Johan_framework
+# JFramework
+Placer le source des classes ou on veut avoir la liste des fonctions par ces classes
--- src/java/etu1933/framework/servlet/FrontServlet.java
+++ src/java/etu1933/framework/servlet/FrontServlet.java
@@ -1,10 +1,13 @@
 package etu1933.framework.servlet;
 
 import etu1933.framework.Mapping;
+import helpers_J.Init;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +17,20 @@ public class FrontServlet extends HttpServlet {
     
     HashMap<String,Mapping> MappingUrls;
     
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String path = getServletContext().getRealPath("/WEB-INF/classes");
+        String package_name = getServletConfig().getInitParameter("package_name");
+        Init init = new Init();
+        try {
+            this.MappingUrls = new HashMap<>();
+            init.setUrl(this.MappingUrls,null, package_name, path);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -34,24 +51,28 @@ public class FrontServlet extends HttpServlet {
             out.println("<h3>" + request.getServletPath()+ "</h3>");
             out.println("<p><u>Map:</u></p>");
             Map<String, String[]> paramMap = request.getParameterMap();
-            for (String paramName : paramMap.keySet()) {
+            for (String paramName : paramMap.keySet()) 
+            {
                 String[] paramValues = paramMap.get(paramName);
-                for (String paramValue : paramValues) {
+                for (String paramValue : paramValues) 
+                {
                     out.println("<h3>" + paramName + " = " + paramValue+ "</h3>");
                 }
             }
+            out.println("<p><u>MappingUrls:</u></p>");
+            out.println( "<h1>"+this.MappingUrls+"</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+    {
         processRequest(request, response);
     }
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+    {
         processRequest(request, response);
     }
 }
